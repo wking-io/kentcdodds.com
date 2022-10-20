20f9ef5a6c0f2ab89d846d8d21bd1c5e43ad9890
Kent C. Dodds <me+github@kentcdodds.com>
2022-10-20 16:32:17 -0600

    make migrate more reliable

--- prisma-postgres/migrate.ts
+++ prisma-postgres/migrate.ts
@@ -26,25 +26,33 @@ async function main() {
   const users = await pg.user.findMany()
   for (const user of users) {
     // eslint-disable-next-line no-await-in-loop
-    await sq.user.create({data: user})
+    await sq.user.upsert({where: {id: user.id}, update: user, create: user})
   }
 
   const sessions = await pg.session.findMany()
   for (const session of sessions) {
     // eslint-disable-next-line no-await-in-loop
-    await sq.session.create({data: session})
+    await sq.session.upsert({
+      where: {id: session.id},
+      update: session,
+      create: session,
+    })
   }
 
   const postReads = await pg.postRead.findMany()
   for (const postRead of postReads) {
     // eslint-disable-next-line no-await-in-loop
-    await sq.postRead.create({data: postRead})
+    await sq.postRead.upsert({
+      where: {id: postRead.id},
+      update: postRead,
+      create: postRead,
+    })
   }
 
   const calls = await pg.call.findMany()
   for (const call of calls) {
     // eslint-disable-next-line no-await-in-loop
-    await sq.call.create({data: call})
+    await sq.call.upsert({where: {id: call.id}, update: call, create: call})
   }
 
   console.log('✅  all finished')
